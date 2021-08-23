f6b3c85f232e69b126b2022de48b040f760678d4
Maharshi Shah <40323681+maharshi66@users.noreply.github.com>
2021-08-23 15:28:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { Button } from "react-bootstrap";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">Primary</Button>
+    </div>
+  );
+}
